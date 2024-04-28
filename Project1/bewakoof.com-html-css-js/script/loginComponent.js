97acbc2826d3292c74d839d1290be042c5dbf483
Arpan Jain <arpanjain231995@gmail.com>
2024-04-28 12:03:49 +0530

    Mock Evaluation

--- Project1/bewakoof.com-html-css-js/script/loginComponent.js
+++ Project1/bewakoof.com-html-css-js/script/loginComponent.js
@@ -1,34 +1,45 @@
 import { dataFetch } from "../script/dataFetch.js";
 
 function loginComponent() {
-    const loginComp = document.createElement("div");
-    loginComp.id = "login-Comp";
+  const loginComp = document.createElement("div");
+  loginComp.id = "login-Comp";
 
-    const loginDivLeft = createLoginDivLeft();
-    const loginDivRight = createLoginDivRight();
+  const loginDivLeft = createLoginDivLeft();
+  const loginDivRight = createLoginDivRight();
+  const otpForm = openOTPPopup();
 
-    loginComp.append(loginDivLeft, loginDivRight);
-    
-    return loginComp;
+  displayLoginForm(true);
+
+  return loginComp;
+
+  function displayLoginForm(showLoginForm) {
+    if (showLoginForm) {
+        loginComp.innerHTML = "";
+        loginComp.append(loginDivLeft, loginDivRight);
+    } else {
+        loginComp.innerHTML = "";
+        loginComp.appendChild(otpForm);
+    }
+  };
 }
 
 function createLoginDivLeft() {
-    const loginDivLeft = document.createElement("div");
-    loginDivLeft.id = "loginDivLeft";
+  const loginDivLeft = document.createElement("div");
+  loginDivLeft.id = "loginDivLeft";
 
-    loginDivLeft.innerHTML = `
+  loginDivLeft.innerHTML = `
         <h2>Welcome to the world of Bewakoof®!</h2>
         <img src="https://images.bewakoof.com/web/group-19-1617704502.png" alt="Bewakoof logo">
     `;
-    
-    return loginDivLeft;
+
+  return loginDivLeft;
 }
 
 function createLoginDivRight() {
-    const loginDivRight = document.createElement("div");
-    loginDivRight.id = "loginDivRight";
+  const loginDivRight = document.createElement("div");
+  loginDivRight.id = "loginDivRight";
 
-    loginDivRight.innerHTML = `
+  loginDivRight.innerHTML = `
         <h1>Log in / Sign up</h1>
         <p>for Latest trends, exciting offers and everything Bewakoof®!</p>
         <form id="loginForm">
@@ -44,53 +55,66 @@ function createLoginDivRight() {
         </form>
     `;
 
-    const loginForm = loginDivRight.querySelector("#loginForm");
-    loginForm.addEventListener('submit', handleLoginSubmit);
-    
-    return loginDivRight;
+  const loginForm = loginDivRight.querySelector("#loginForm");
+  loginForm.addEventListener("submit", handleLoginSubmit);
+
+  return loginDivRight;
 }
 
 async function handleLoginSubmit(event) {
-    event.preventDefault();
-    const mobileNumberInput = document.getElementById('mobileNumberInput');
-    const mobileNumber = mobileNumberInput.value;
+  event.preventDefault();
+  const mobileNumberInput = document.getElementById("mobileNumberInput");
+  const mobileNumber = mobileNumberInput.value;
 
-    const userData = await userDataFetch();
+  const userData = await userDataFetch();
 
-    const userExists = userData.some(user => user.mobileNo === mobileNumber)
+  const userExists = userData.some((user) => user.mobileNo === mobileNumber);
 
-    if(userExists) {
-        alert("Mobile number exists")
-        openOTPPopup(mobileNumber);
-    } else {
-        alert("Mobile number dose not exists")
-    }
+  if (userExists) {
+    alert("Mobile number exists");
+    displayLoginForm(false);
+    openOTPPopup(mobileNumber);
+  } else {
+    alert("Mobile number dose not exists");
+  }
 }
 
 async function openOTPPopup(number) {
+  const otpForm = document.createElement("div");
+  otpForm.id = "otpForm";
+  otpForm.innerHTML = `
+        <h2>Verify with OTP</h2>
+        <form>
+            <label>Sent to</label>
+            <input type="text" placeholder=${number} disabled>
+        </form>
+        <div>
+            <p>Enter OTP</p>
+            <form>
+                <div class="otp-box">
+                    <input name="otp1" type="tel" autocomplete="off" class="otpInput undefined " tabindex="1" maxlength="1" placeholder="" value="">
+                    <input name="otp2" type="tel" autocomplete="off" class="otpInput undefined " tabindex="2" maxlength="1" placeholder="" value="">
+                    <input name="otp3" type="tel" autocomplete="off" class="otpInput undefined " tabindex="3" maxlength="1" placeholder="" value="">
+                    <input name="otp4" type="tel" autocomplete="off" class="otpInput undefined " tabindex="4" maxlength="1" placeholder="" value="">
+                </div>
+                <div>
+                    <button>
+                        RESEND OTP
+                    </button>
+                </div>
+                <button disabled type="submit">Login</button>
+            </form>
+        </div>
+        `;
+  document.body.appendChild(otpForm);
 
-    const otpForm = document.createElement("div");
-    const heading = document.createElement("h2");
-    heading.innerText = "Verify with OTP";
-    const numberForm = document.createElement("form");
-    const numberInput = document.createElement("input");
-    numberInput.placeholder = number;
-    numberInput.disabled = true;
-    const numberLabel = document.createElement("label");
-    numberLabel.textContent = "Sent to";
-
-
-    numberForm.append(numberLabel, numberInput);
-    otpForm.append(heading, numberForm);
-    document.body.appendChild(otpForm);
-
-    return otpForm;
+  return otpForm;
 }
 
 async function userDataFetch() {
-    const response = await fetch("http://localhost:3000/user");
-    const data = await response.json();
-    return data
+  const response = await fetch("http://localhost:3000/user");
+  const data = await response.json();
+  return data;
 }
 
 export { loginComponent };
